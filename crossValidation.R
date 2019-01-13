017ec0e620fa64b2168758c8b71b7f0f608fe1e5
Michele Pesce <14021561@telecomitalia.local>
2019-01-13 18:28:04 +0100

    13-01-19

--- crossValidation.R
+++ crossValidation.R
@@ -21,5 +21,37 @@ tt <- colttests(x, y)
 #RISPOSTA Q3
 ind <- which(tt$p.value<0.01)
 ind
+#risposta q4
+x_subset<- x[ ,ind]
+fit <- train(x_subset, y, method = "glm")
+fit$results
+#risposta q5
+fit <- train(x_subset, y, method = "knn", tuneGrid = data.frame(k = seq(101, 301, 25)))
+ggplot(fit)
 
-x_subset_R <- x[ ,ind]
+
+#risposta q6
+library(dslabs)
+data("tissue_gene_expression")
+x<-tissue_gene_expression$x
+y<-tissue_gene_expression$y 
+
+train_index <- createDataPartition(y, times = 1, list = FALSE)
+train_set_x <- x[train_index, ]
+test_set_x <- x[-train_index, ]
+train_set_y <- y[train_index]
+test_set_y <- y[-train_index]
+set.seed(1)
+ks <- c(1, 3, 5, 7, 9, 11)
+accuracy <- map_df(ks, function(k){
+  
+  fit <- knn3(train_set_x,train_set_y,  k = k)
+  y_hat <- predict(fit, test_set_x, type = "class") 
+  test_error<- confusionMatrix(data=y_hat,reference = test_set_y,mode="everything")$overall["Accuracy"]
+  list(k=k,test=test_error)
+})
+as.data.frame(accuracy)
+
+fit <- train(train_set_x, train_set_y, method = "knn", tuneGrid = data.frame(k = seq(1, 45, 2)))
+ggplot(fit)
+
